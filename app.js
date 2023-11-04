e32dc1a1774953e5e7d04f8a01b48621a86ad760
Clever AKANIMOH <crushclever1@gmail.com>
2023-11-04 21:10:43 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,16 +3,15 @@ const qr = document.querySelector('#qr');
 
 data.onchange = e => {
         qr.innerHTML= '';
-        const href = data.value;
+        const text = data.value;
         // size of the image
-        const size = 330;
-        const sizeh = 269;
+        const width = 330;
+        const height = 269;
 
         new QRCode(qr, {
-            text: href,
-            width: size,
-            height: sizeh,
-
+            text,
+            width,
+            height,
             colorDark: '#040404',
             colorLight: '#e9eef4'
         })
